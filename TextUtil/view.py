7efd400bbc7bec98a8ee4e023816167f261188b1
Vivek Bhardwaj <vivekbhardwaj0067@gmail.com>
2019-12-21 16:43:51 +0530

    Made n index page that would be rendered when the website loads

--- TextUtil/view.py
+++ TextUtil/view.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Made the view file')
+    return render(request,'index.html')
